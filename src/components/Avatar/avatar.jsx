25d947c878f928988bf4f076d2a1911d8b1bde92
Fedor Rushkevich <fedrushkevich@gmail.com>
2024-04-04 14:46:18 +0300

    (EA-01): fix App.js

--- src/components/Avatar/avatar.jsx
+++ src/components/Avatar/avatar.jsx
@@ -1,24 +1,15 @@
-class avatar extends React.Component {
-constructor() {
-    this.state={
-      isAvatarsVisible: false,
-    }
-}
-    handleshowavatars() {
-    this.state.isAvatarsVisible = !this.state.isAvatarsVisible;
-}
+import React from 'react';
+
+class Avatar extends React.Component {
+  render() {
+    const { userName, imageSrc } = this.props;
     return (
-          <button onClick={handleshowavatars()}>Click!!!</button>
-           {if(isAvatarsVisible) {
-               <div>
-                  <div>{this.props.userName}</div>
-                   <img src={props.imageSrc}></img>
-               </div>
-           } else {
-               <div>Here should be list of avatars</div>
-           }
-           }
-           )
+      <div>
+        <div>{userName}</div>
+        <img src={imageSrc} alt="Avatar" />
+      </div>
+    );
+  }
 }
 
-export  default avatar;
+export default Avatar;
